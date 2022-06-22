1c0c342db300961c21bee0815e2c898ae0ed11e8
roie <roie058@gmail.com>
2022-06-22 16:39:56 +0300

    correction export

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,52 @@ import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
 import { AuthContext } from "../lib/context/auth-context";
 import { useAuth } from "../lib/hooks/auth-hook";
+import { Fragment } from "react";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   const { login, logout, isAdmin, userId, token } = useAuth();
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: !!token,
-        token: token,
-        userId: userId,
-        isAdmin: isAdmin,
-        login: login,
-        logout: logout,
-      }}
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthContext.Provider>
+    <Fragment>
+      <Head>
+        <link rel="icon" href="/logo.jpg" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no"
+        />
+
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content=" מצבות ליבוביץ
+      
+        מצבות ואנדרטאות בכל רחבי הארץ
+        
+        ! שמים את השירות והאמינות מעל הכל
+        
+        עסק משפחתי שמתמחה בתחום כבר למעלה מ40 שנה
+     "
+        />
+        <link rel="apple-touch-icon" href="/logo.jpg" />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <title>מצבות ליבוביץ</title>
+      </Head>
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: !!token,
+          token: token,
+          userId: userId,
+          isAdmin: isAdmin,
+          login: login,
+          logout: logout,
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthContext.Provider>
+    </Fragment>
   );
 }
 
