7fbd8248cc1e2f45418643839a933a85141eb594
roie <roie058@gmail.com>
2022-06-22 12:56:10 +0300

    correction export

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
+import Layout from "../components/layout/Layout";
+import { AuthContext } from "../lib/context/auth-context";
+import { useAuth } from "../lib/hooks/auth-hook";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { login, logout, isAdmin, userId, token } = useAuth();
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        isAdmin: isAdmin,
+        login: login,
+        logout: logout,
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
